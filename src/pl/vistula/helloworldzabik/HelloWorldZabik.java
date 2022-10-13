a62d395cf0192dd6b639b65ed4a39eafe6f03da7
Your Name <you@example.com>
2022-10-13 17:23:44 +0200

    Drugie wyświetlenie

--- src/pl/vistula/helloworldzabik/HelloWorldZabik.java
+++ src/pl/vistula/helloworldzabik/HelloWorldZabik.java
@@ -1,2 +1,8 @@
-package pl.vistula.helloworldzabik;public class HelloWorldZabik {
+package pl.vistula.helloworldzabik;
+
+public class HelloWorldZabik {
+    public static void main(String[] args) {
+        System.out.println("Hello World Zabik");
+        System.out.println("Ence Pence");
+    }
 }
